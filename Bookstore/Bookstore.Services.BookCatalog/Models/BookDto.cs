847bb248bff5b948418789171b3b21be6d4d581d
Kamil <kamilsalega@gmail.com>
2021-02-08 05:58:34 +0100

    001 - Update BookDto

--- Bookstore/Bookstore.Services.BookCatalog/Models/BookDto.cs
+++ Bookstore/Bookstore.Services.BookCatalog/Models/BookDto.cs
@@ -7,6 +7,9 @@ namespace Bookstore.Services.BookCatalog.Models
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime Date { get; set; }
         public Guid AuthorId { get; set; }
         public Guid CategoryId { get; set; }
         public string CategoryName { get; set; }
