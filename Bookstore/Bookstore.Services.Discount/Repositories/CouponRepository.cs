23fe257d3697b88c782920c1b198b8f2df616306
Kamil <kamilsalega@gmail.com>
2021-02-10 21:03:42 +0100

    003 - Create Coupon Repository

--- Bookstore/Bookstore.Services.Discount/Repositories/CouponRepository.cs
+++ Bookstore/Bookstore.Services.Discount/Repositories/CouponRepository.cs
@@ -1,7 +1,42 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Services.Discount.DbContexts;
+using Bookstore.Services.Discount.Entities;
+using Bookstore.Services.Discount.Repositories.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 namespace Bookstore.Services.Discount.Repositories
 {
-    public class CouponRepository
+    public class CouponRepository : ICouponRepository
     {
-        
+        private readonly DiscountDbContext _discountDbContext;
+
+        public CouponRepository(DiscountDbContext discountDbContext)
+        {
+            _discountDbContext = discountDbContext;
+        }
+
+        public async Task<Coupon> GetCouponByCode(string couponCode)
+        {
+            return await _discountDbContext.Coupons.Where(x => x.Code == couponCode).FirstOrDefaultAsync();
+        }
+
+        public async Task UseCoupon(Guid couponId)
+        {
+            var couponToUpdate =
+                await _discountDbContext.Coupons.Where(x => x.CouponId == couponId).FirstOrDefaultAsync();
+
+            if (couponToUpdate == null)
+                throw new Exception();//TODO custom exception
+
+            couponToUpdate.AlreadyUsed = true;
+            await _discountDbContext.SaveChangesAsync();
+        }
+
+        public async Task<Coupon> GetCouponById(Guid couponId)
+        {
+            return await _discountDbContext.Coupons.Where(x => x.CouponId == couponId).FirstOrDefaultAsync();
+        }
     }
 }
