ce3b906df507db6a2080f487907da377df22c3e2
Kamil <kamilsalega@gmail.com>
2021-02-10 21:26:33 +0100

    003 - Create Coupon Profiles

--- Bookstore/Bookstore.Services.Discount/Profiles/CouponProfile.cs
+++ Bookstore/Bookstore.Services.Discount/Profiles/CouponProfile.cs
@@ -1,7 +1,14 @@
+using AutoMapper;
+using Bookstore.Services.Discount.Entities;
+using Bookstore.Services.Discount.Models;
+
 namespace Bookstore.Services.Discount.Profiles
 {
-    public class CouponProfile
+    public class CouponProfile : Profile
     {
-        
+        public CouponProfile()
+        {
+            CreateMap<Coupon, CouponDto>().ReverseMap();
+        }
     }
 }
