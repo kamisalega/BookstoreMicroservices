ae6b23ffcc6e6fe5a11128acbf73af3d0de328c3
Kamil <kamilsalega@gmail.com>
2021-03-07 19:05:52 +0100

    005 - Intetegration Masseges Bus

--- Bookstore/Bookstore.Services.Ordering/Controllers/OrderController.cs
+++ Bookstore/Bookstore.Services.Ordering/Controllers/OrderController.cs
@@ -1,7 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using Bookstore.Services.Ordering.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Bookstore.Services.Ordering.Controllers
 {
-    public class OrderController
+    public class OrderController : Controller
     {
-        
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        [HttpGet("user/{userId}")]
+        public async Task<IActionResult> List(Guid userId)
+        {
+            var orders = await _orderRepository.GetOrdersForUser(userId);
+            return Ok(orders);
+        }
     }
 }
---
+++ Bookstore/Integration/Bookstore.Integration.Messages/IntegrationBaseMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Bookstore.Integration.Messages
+{
+    public class IntegrationBaseMessage
+    {
+        public Guid Id { get; set; }
+        public DateTime CreationDateTime { get; set; }
+    }
+}
