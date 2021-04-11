cf6629135c2b73b0cfb2e3ba7b2fc750d2936a21
Kamil <kamilsalega@gmail.com>
2021-04-11 02:22:46 +0200

    001 - Improvement of book catalog

--- Bookstore/Bookstore.Services.BookCatalog/Entities/Author.cs
+++ Bookstore/Bookstore.Services.BookCatalog/Entities/Author.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bookstore.Services.BookCatalog.Entities
 {
     public class Author
     {
+        [Required]
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
