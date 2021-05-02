437e91c65fcc794f0b79c16b08a266755e190add
Kamil <kamilsalega@gmail.com>
2021-05-02 03:07:49 +0200

    008 - Adding Support for the Token Exchange Grant

---
+++ Bookstore/Bookstore.Services.Identity/Services/TokenExchangeExtensionGrantValidator.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Validation;
+
+namespace Bookstore.Services.Identity.Services
+{
+    public class TokenExchangeExtensionGrantValidator : IExtensionGrantValidator
+    {
+        private readonly ITokenValidator _validator;
+        public string GrantType => "urn:ietf:params:oauth:grant-type:token-exchange";
+        private string _accessTokenType => "urn:ietf:params:oauth:token-type:access_token";
+
+        public TokenExchangeExtensionGrantValidator(ITokenValidator validator)
+        {
+            _validator = validator ?? throw new ArgumentNullException(nameof(validator));
+        }
+        public async Task ValidateAsync(ExtensionGrantValidationContext context)
+        {
+            var requestedGrant = context.Request.Raw.Get("grant_type");
+            if (string.IsNullOrWhiteSpace(requestedGrant) || requestedGrant != GrantType)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant,
+                    "Invalid grant.");
+                return;
+            }
+
+            var subjectToken = context.Request.Raw.Get("subject_token");
+            if (string.IsNullOrWhiteSpace(subjectToken))
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant,
+                    "Subject token missing.");
+                return;
+            }
+
+            var subjectTokenType = context.Request.Raw.Get("subject_token_type");
+            if (string.IsNullOrWhiteSpace(subjectTokenType))
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant,
+                    "Subject token type missing.");
+                return;
+            }
+
+            if (subjectToken != _accessTokenType)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant,
+                    "Subject token type invalid.");
+                return;
+            }
+
+            var result = await _validator.ValidateAccessTokenAsync(subjectToken);
+            if (result.IsError)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant,
+                    "Subject token invalid.");
+                return;
+            }
+
+            var subjectClaim = result.Claims.FirstOrDefault(c => c.Type == "sub");
+            if (subjectClaim == null)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidRequest,
+                    "Subject token must contain sub value.");
+                return;
+            }
+
+            context.Result = new GrantValidationResult(subjectClaim.Value,
+                "access_token",
+                result.Claims);
+
+            return;
+        }
+    }
+}
+
